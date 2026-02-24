d078a4c10fce3d5752ec54acfad97a548b32f609
Esraa Elnagdi <38186163+elnagdie@users.noreply.github.com>
2026-02-23 23:49:34 -0500

    feat: add stranger things and harry potter theme styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/stranger-things.css'
+import './styles/harry-potter.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
